4f5405450a1280c6e5ef4d2e41022ac0278be6f0
Egor Miroevsky <egor932005@mail.ru>
2025-08-07 17:12:13 +0300

    refactor: update project structure

---
+++ backend/app/core/exceptions.py
@@ -0,0 +1,18 @@
+from fastapi import Request, status
+from fastapi.responses import JSONResponse
+from sqlalchemy.exc import SQLAlchemyError
+
+
+def sqlalchemy_exception_handler(request: Request, exc: SQLAlchemyError):
+    """Обработчик всех ошибок SQLAlchemy"""
+    return JSONResponse(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        content={
+            'message': 'Database error'
+        },
+    )
+
+
+def register_exception_handlers(app):
+    """Функция для регистрации всех обработчиков ошибок"""
+    app.exception_handler(SQLAlchemyError)(sqlalchemy_exception_handler)
