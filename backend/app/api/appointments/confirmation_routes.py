59cbc7ffb50ed753e8936893158f4a167063b3c4
Egor Miroevsky <egor932005@mail.ru>
2025-09-20 17:15:46 +0300

    fix: change relative import

--- backend/app/api/appointments/confirmation_routes.py
+++ backend/app/api/appointments/confirmation_routes.py
@@ -3,12 +3,12 @@ from sqlalchemy import update
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import Annotated
 
-from ...core.auth import verify_token
-from ...core.schemas import OKModel, ConfirmationCode
-from ...db.models import Appointment
-from ...db.postgresql import get_session
-from ...db.queries import select_one
-from ...ws.appointments.notifications import ws_appointments_manager
+from core.auth import verify_token
+from core.schemas import OKModel, ConfirmationCode
+from db.models import Appointment
+from db.postgresql import get_session
+from db.queries import select_one
+from ws.appointments.notifications import ws_appointments_manager
 
 
 confirmation_router = APIRouter()
